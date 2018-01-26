6bac05b47bba4b45c9b9912a75d0d1eec2350cdf
Vladimir <33662107+buain@users.noreply.github.com>
2018-01-26 17:17:53 +0300

    Initial commit

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String n = "\nмоя строка 1";
+            Console.WriteLine(n);
+            String t = "\tмоя строка 2";
+            Console.WriteLine(t);
+            String a = "\aмоя строка 3";
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+}
